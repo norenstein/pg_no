a52b1762fc3a693bcfa5a351a5adaf9f5fad399b
norenstein <33520761+norenstein@users.noreply.github.com>
2018-01-11 09:53:11 -0500

    Add files via upload

---
+++ personality_NO.py
@@ -0,0 +1,31 @@
+import time 
+name = "Nick"
+state = "California"
+book = "The Giver"
+city = "Chicago"
+tvshow = "sports center"
+
+videogame = "Ghost recon"
+movie = "Miracle"
+sportsteam = "Rangers"
+print(name + " likes to visit " + state + " and " + city)
+print (name + " watches " + tvshow + " and reads " + book)
+
+
+print("What's your favorite color?")
+color = input()
+
+
+if color == "blue":
+    print("Blue is my favorite!")
+else:
+    print(color + " is nice too. " )
+
+
+
+pring("What's you favorite sportsteam?")
+sportsteam = input()
+
+time.sleep(60)
+
+
